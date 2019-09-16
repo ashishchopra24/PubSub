ce352a20422e1bb9b4bc6f4f11ebd1bc63965659
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2019-09-16 22:21:30 +0530

    first commit

---
+++ src/main/java/com/pubsub/project/Publisher/HRApp.java
@@ -0,0 +1,33 @@
+package com.pubsub.project.Publisher;
+
+import com.pubsub.project.Employee;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import javax.jms.JMSContext;
+import javax.jms.Topic;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class HRApp {
+    public static void main(String[] args) throws NamingException {
+        InitialContext initialContext=new InitialContext();
+        Topic topic=(Topic)initialContext.lookup("topic/myTopic");
+
+        try(ActiveMQConnectionFactory cf=new ActiveMQConnectionFactory();
+            JMSContext jmsContext=cf.createContext())
+        {
+            Employee emp=new Employee();
+            emp.setId(2343);
+            emp.setFirstName("ashish");
+            emp.setLastName("chopra");
+            emp.setEmail("ashish.chopra@e2open.com");
+            emp.setPhone("374991023");
+
+            jmsContext.createProducer().send(topic,emp);
+
+            System.out.println("Message sent");
+
+
+        }
+    }
+}
---
+++ src/main/java/com/pubsub/project/Subscribers/SecurityApp.java
@@ -0,0 +1,32 @@
+package com.pubsub.project.Subscribers;
+
+import com.pubsub.project.Employee;
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.sql.SQLOutput;
+
+public class SecurityApp {
+
+    public static void main(String[] args) throws NamingException, JMSException {
+        System.out.println("App started");
+        InitialContext initialContext;
+        initialContext = new InitialContext();
+        Topic topic=(Topic)initialContext.lookup("topic/myTopic");
+
+        try(ActiveMQConnectionFactory cf=new ActiveMQConnectionFactory();
+            JMSContext jmsContext=cf.createContext())
+        {
+            JMSConsumer consumer=jmsContext.createConsumer(topic);
+            Message message=consumer.receive();
+            Employee employee=message.getBody(Employee.class);
+            System.out.println(employee.getFirstName());
+
+
+
+
+        }
+    }
+}
